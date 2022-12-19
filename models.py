742a9ded7a8441351bcd73288c40f1c6fafd8849
githubfra <diazcabellofrancisca@gmail.com>
2022-12-19 01:39:40 +0000

    first commit

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Todos(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.String(500))
+    done = db.Column(db.Boolean, default=True, nullable=False)   
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body":self.body,
+            "done": self.done,
+            
+        }
